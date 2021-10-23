695a54b2f48715efc1cd7e5cc5571f1abd0443c2
Klara Stantič <stantic.klara@gmail.com>
2021-10-23 18:32:42 +0200

    začetki spletnega vmesnika in prve html predloge

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,2 +1,24 @@
 import bottle
+#import os
+#from datetime import date
+#from model import Model, Zapisek, Dogodek, Sklop_vaj, Skladba, Vaja
 
+#DATOTEKA = "model.json"
+#try:
+#    moj_model = Model.preberi_iz_datoteke(DATOTEKA)
+#except FileNotFoundError:
+#    moj_model = Model()
+
+@bottle.get('/')
+def osnovna_stran():
+    return bottle.template(
+        "osnovna_stran.html", 
+
+    )
+
+
+@bottle.error(404)
+def error_404():
+    return "Ta spletna stran ne obstaja!"
+
+bottle.run(reloader=True, debug=True)
