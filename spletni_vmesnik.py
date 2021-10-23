62fa81dd818b52f035f2f318249a642c3b072457
Klara Stantič <stantic.klara@gmail.com>
2021-10-23 21:24:46 +0200

    Začetek spletnega vmesnika in prijavne funkcije

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -1,24 +1,79 @@
 import bottle
-#import os
-#from datetime import date
-#from model import Model, Zapisek, Dogodek, Sklop_vaj, Skladba, Vaja
+import os
+from datetime import date
+from model import Model, Zapisek, Dogodek, Sklop_vaj, Skladba, Vaja
 
-#DATOTEKA = "model.json"
-#try:
-#    moj_model = Model.preberi_iz_datoteke(DATOTEKA)
-#except FileNotFoundError:
-#    moj_model = Model()
+
+def nalozi_uporabnikov_model():
+    uporabnisko_ime = bottle.request.get_cookie("uporabnisko_ime")
+    if uporabnisko_ime:
+        return Model.preberi_iz_datoteke(uporabnisko_ime)
+    else:
+        bottle.redirect("/prijava/")
+
+
+def shrani_uporabnikov_model(moj_model):
+    uporabnisko_ime = bottle.request.get_cookie("uporabnisko_ime")
+    moj_model.shrani_v_datoteko(uporabnisko_ime)
+
+
+@bottle.get("/registracija/")
+def registracija_get():
+    return bottle.template("registracija.html", napake={}, polja={}, uporabnisko_ime=None)
+
+
+@bottle.post("/registracija/")
+def registracija_post():
+    uporabnisko_ime = bottle.request.forms.getunicode("uporabnisko_ime")
+    if os.path.exists(uporabnisko_ime):
+        napake = {"uporabnisko_ime": "Uporabniško ime že obstaja."}
+        return bottle.template("registracija.html", napake=napake, polja={"uporabnisko_ime": uporabnisko_ime}, uporabnisko_ime=None)
+    else:
+        bottle.response.set_cookie(
+            "uporabnisko_ime", uporabnisko_ime, path="/")
+        Model().shrani_v_datoteko(uporabnisko_ime)
+        bottle.redirect("/")
+
+
+@bottle.get("/prijava/")
+def prijava_get():
+    return bottle.template("prijava.html", napake={}, polja={}, uporabnisko_ime=None)
+
+
+@bottle.post("/prijava/")
+def prijava_post():
+    uporabnisko_ime = bottle.request.forms.getunicode("uporabnisko_ime")
+    if not os.path.exists(uporabnisko_ime):
+        napake = {"uporabnisko_ime": "Uporabniško ime ne obstaja."}
+        return bottle.template("prijava.html", napake=napake, polja={"uporabnisko_ime": uporabnisko_ime}, uporabnisko_ime=None)
+    else:
+        bottle.response.set_cookie(
+            "uporabnisko_ime", uporabnisko_ime, path="/")
+        bottle.redirect("/")
+
+
+@bottle.post("/odjava/")
+def odjava_post():
+    bottle.response.delete_cookie("uporabnisko_ime", path="/")
+    print("Piškotek je bil uspešno pobrisan.")
+    bottle.redirect("/")
+
 
 @bottle.get('/')
 def osnovna_stran():
     return bottle.template(
-        "osnovna_stran.html", 
-
+        "baza.html",
     )
 
 
+@bottle.get('/Zapiski/')
+def zapiski():
+    pass
+
+
 @bottle.error(404)
-def error_404():
-    return "Ta spletna stran ne obstaja!"
+def error_404(error):
+    return "Ta stran ne obstaja!"
+
 
 bottle.run(reloader=True, debug=True)
